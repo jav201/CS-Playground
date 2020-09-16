35dd26d35c82e1dc1230366ccad6ce2c884d1897
Javier Granados <jjgh89@msn.com>
2020-09-16 18:26:04 -0500

    working merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -2,15 +2,21 @@ import numpy as np
 
 def merge(array_1, array_2):
     out_list = []
+    # The order goes from max to min with this transormation
+    array_1 = array_1[::-1]
+    array_2 = array_2[::-1]
     while len(array_1) > 0 and len(array_2) > 0:
-        if array_1[-1] > array_2[-1]:
+        # Append the smallest value at the end of the list on outlist
+        if array_1[-1] < array_2[-1]:
             out_list.append(array_1.pop())
         else:
             out_list.append(array_2.pop())
+    # if a list is emptied, the rest of the other gets appended on the outlist
+    # the lists are ordered from max to min, they need to be from min to max after transformation
     if len(array_1) == 0:
-        out_list = out_list + array_2
+        out_list = out_list + array_2[::-1]
     else:
-        out_list = out_list + array_1
+        out_list = out_list + array_1[::-1]
     return out_list
 
 
@@ -23,6 +29,6 @@ def merge_sort(array_3):
         right_l = array_3[half_point::]
         return merge(merge_sort(left_l), merge_sort(right_l))
 
-arr = list(np.random.randint(10, size=10))
+arr = list(np.random.randint(99, size=200))
 print(arr)
 print(merge_sort(arr))
