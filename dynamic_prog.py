9f004d90b7ce76f137dafe05fa5a6dda8e4a7d61
Javier Granados <jjgh89@msn.com>
2021-07-18 15:57:36 -0500

    Added fancy printing and comments

--- dynamic_prog.py
+++ dynamic_prog.py
@@ -1,10 +1,11 @@
 # Longest common substring
-word1 = 'caracuaro'
-word2 = 'paztcuaro'
-word2 = 'jerecuaro'
+word1 = 'acomadre'
+word2 = 'comadreja'
+word3 = 'jerecuaro'
 
 word_matrix = [ [ 0 for x in range(len(word2)) ] for _ in range(len(word1)) ]
 
+# If a letter match , add one to the position and carry the diagonal value from the previous row and column
 for i in range(len(word1)):
     for j in range(len(word2)):
         # Check if the letter matches
@@ -12,5 +13,11 @@ for i in range(len(word1)):
             word_matrix[i][j] = 1 + word_matrix[i-1][j-1]
         else:
             word_matrix[i][j] = 0
-print(word_matrix)
+
+# Some fancy printing            
+print('  ',end='')
+for i in word2: print(' '+i+' ', end='')
+print('')
+for i,l in zip(word_matrix, list(word1)): print(l,i)
+print('\n'+'Max value or highest similar coefficient:')
 print(max([max(x) for x in word_matrix]))
